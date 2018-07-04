3464253ecb6b940ed01163b780f09c4586076a75
John William Davis <jwdstreetphotog@gmail.com>
2018-07-04 01:57:18 -0700

    Added Several More Files
    Some more simple code files from my work with Treehouse.

---
+++ Treehouse Java Stuff/TreeStory3.java
@@ -0,0 +1,32 @@
+import java.io.Console;
+ 
+public class TreeStory {
+    
+    public static void main(String[] args) {
+        Console console = System.console();
+        /*  Some terms:
+            noun - Person, place or thing
+            verb - An action
+            adjective - A description used to modify or describe a noun
+            Enter your amazing code here!
+        */
+        // __Name__ is a __adjective__ __noun__. They are always __adverb__ __verb__.
+        int age = 12;
+        if (age < 13)  {
+            //my first simple if statement in java
+            //insert exit code
+            console.printf("Sorry, you must be atleast 13 to use this program.");
+            System.exit(0);
+        }
+        String name = console.readLine("Enter a name:  ");
+        String adjective = console.readLine("Enter an adjective:  ");
+        String noun = console.readLine("Enter a noun:  ");
+        String adverb = console.readLine("Enter an adjective:  ");
+        String verb = console.readLine("Enter a verb ending with -ing:  ");
+        
+        console.printf("Your TreeStory:\n ------------------------\n");
+        console.printf("%s is a %s %s.  ", name, adjective, noun);
+        console.printf("They are always %s %s.\n", adverb, verb);
+    }
+    
+}
